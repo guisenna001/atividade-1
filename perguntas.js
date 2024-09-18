6802377240c831da93c001adea06ee7e5e61ba4e
guisenna001 <142246411+guisenna001@users.noreply.github.com>
2024-09-18 01:53:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é o valor de "X", resolva o calculo: 2x+39=456?',
+    'O valor de "X" é 208,5'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se escreve "Ela mora sozinha" em Inglês?',
+    '"Ela mora sozinha" em ingles é She lives alone'
+)
